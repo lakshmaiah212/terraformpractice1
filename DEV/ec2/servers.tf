e9d674a50493b1a75f48fdec239274d54fa1867b
Your Name <you@example.com>
2019-10-13 15:24:59 +0530

    added server

--- DEV/ec2/servers.tf
+++ DEV/ec2/servers.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "webserver" {
   count = 1
   ami           = "ami-04b9e92b5572fa0d1"
   instance_type = "t2.micro"
-  key_name = "endpoint"
+  key_name = "ubuntu.pem"
   subnet_id = "${element(var.subnets_id,count.index)}"
   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
   associate_public_ip_address = true
