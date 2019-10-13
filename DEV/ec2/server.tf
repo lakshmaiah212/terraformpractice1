8aeb4bd9b3cd79bb1f6061eb0833ae5b542652cd
Your Name <you@example.com>
2019-10-13 15:42:43 +0530

    changedthe server configuration

--- DEV/ec2/server.tf
+++ DEV/ec2/server.tf
@@ -1,15 +1,11 @@
 resource "aws_instance" "web" {
   count = 1
-  ami           = "ami-0b69ea66ff7391e80"
+  ami           = "ami-00eb20669e0990cb4"
   instance_type = "t2.micro"
-  key_name = "endpoint"
   subnet_id = "${element(var.subnets_id,count.index)}"
   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
-  associate_public_ip_address = true
 
   tags = {
     Name = "webserver"
-    project_name = "${var.project_name}"
-    project_env = "${var.project_env}"
   }
 }
