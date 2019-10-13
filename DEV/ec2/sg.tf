18ac19b0a4aa6701c98e287e9f75c5d72ca138ef
Your Name <you@example.com>
2019-10-13 18:23:13 +0530

    changesinsg

--- DEV/ec2/sg.tf
+++ DEV/ec2/sg.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "ssh" {
   name        = "allow_ssh"
   description = "Allow ssh inbound traffic"
-  vpc_id      = "${var.vpc_id.id}"
+  vpc_id      = "${var.vpc_id}"
 
   ingress {
     from_port   = 22
