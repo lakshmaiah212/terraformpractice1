75af1be5270673f32e39c5a01a9a68ce854466bc
Your Name <you@example.com>
2019-10-13 15:50:43 +0530

    keychangesinserver

--- DEV/ec2/server.tf
+++ DEV/ec2/server.tf
@@ -2,11 +2,14 @@ resource "aws_instance" "web" {
   count = 1
   ami           = "ami-00eb20669e0990cb4"
   instance_type = "t2.micro"
-  subnet_id = "${element(var.subnets_id,count.index)}"
+  subnet_id = "${element(var.subnets_id, count.index)}"
   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
-  key_name = "endpoint"
+
 
   tags = {
     Name = "webserver"
+    project_name = "${var.project_name}"
+    project_env = "${var.project_env}"
+    created_with = "Terraform"
   }
 }
