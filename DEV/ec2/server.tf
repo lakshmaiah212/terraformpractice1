54e808a64507eddd399389104ad6c4ecb1d7c8ee
Your Name <you@example.com>
2019-10-13 15:43:36 +0530

    addedkeyname

--- DEV/ec2/server.tf
+++ DEV/ec2/server.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   subnet_id = "${element(var.subnets_id,count.index)}"
   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+  key_name = "endpoint"
 
   tags = {
     Name = "webserver"
