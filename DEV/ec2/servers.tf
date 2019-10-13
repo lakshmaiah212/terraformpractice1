982fd932317292cd2514af3d413b1bf7c8fa0d13
Your Name <you@example.com>
2019-10-13 15:32:39 +0530

    changedkeyname

--- DEV/ec2/servers.tf
+++ DEV/ec2/servers.tf
@@ -1,9 +1,8 @@
-
 resource "aws_instance" "webserver" {
   count = 1
   ami           = "ami-0b69ea66ff7391e80"
   instance_type = "t2.micro"
-  key_name = "ubuntu.pem"
+  key_name = "endpoint"
   subnet_id = "${element(var.subnets_id,count.index)}"
   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
   associate_public_ip_address = true
