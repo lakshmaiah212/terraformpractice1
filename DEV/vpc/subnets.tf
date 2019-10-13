fe25076a2ef8c329485ffb998d052f6fc68c8c5b
Your Name <you@example.com>
2019-10-13 14:52:14 +0530

    changesin the count publicassociation

--- DEV/vpc/subnets.tf
+++ DEV/vpc/subnets.tf
@@ -33,7 +33,7 @@ resource "aws_route_table" "publicroutetable" {
 ### route table association #######
 
 resource "aws_route_table_association" "publicroutetableassociation" {
-  count = "${data.aws_availability_zones.available.names}"
+  count = "${length(data.aws_availability_zones.available.names)}"
   subnet_id      = "${element(aws_subnet.publicsubnets.*.id,count.index)}"
   route_table_id = "${aws_route_table.publicroutetable.id}"
 }
