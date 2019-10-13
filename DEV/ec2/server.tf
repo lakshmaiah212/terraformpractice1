b36b36019b22fb1c5487e6a8d3705ab9990eab75
Your Name <you@example.com>
2019-10-13 18:32:14 +0530

    changesinami

--- DEV/ec2/server.tf
+++ DEV/ec2/server.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   count = 1
-  ami           = "ami-00eb20669e0990cb4"
+  ami           = "ami-0b69ea66ff7391e80"
   instance_type = "t2.micro"
   subnet_id = "${element(var.subnets_id, count.index)}"
   vpc_security_group_ids = ["${aws_security_group.ssh.id}"]
