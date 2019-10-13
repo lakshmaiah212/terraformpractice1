6d7d1cdee7f05b5791f490498ee67761c9169b2f
Your Name <you@example.com>
2019-10-13 16:49:26 +0530

    changes in secutity group

--- DEV/ec2/server.tf
+++ DEV/ec2/server.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "web" {
+  count = 1
   ami           = "ami-00eb20669e0990cb4"
   instance_type = "t2.micro"
-  subnet_id = "${var.subnets_id}"
-  vpc_security_group_ids = "${aws_security_group.ssh.id}"
+  subnet_id = "${element(var.subnets_id,count.index)}"
+  vpc_security_group_ids = ["${aws_security_group.ssh.id}"]
   key_name = "endpoint"
 
   tags = {
